0e2130ee00addf396a395aa2c94d030774ab7c73
Adam <Adam@DESKTOP-GM6IUPR>
2022-02-14 15:59:54 -0300

    Agrego Unit Test de InvoiceItems

---
+++ IngresoSML2.UnitTests/InvoiceItemsTest.cs
@@ -0,0 +1,50 @@
+﻿using IngresoSML2.Business;
+using IngresoSML2.Entities;
+using IngresoSML2.Entities.DTOS;
+using IngresoSML2.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IngresoSML2.UnitTests
+{
+    public class InvoiceItemsTest
+    {
+
+        private readonly Mock<IInvoiceItemsInterface> mock = new Mock<IInvoiceItemsInterface>();
+        private InvoiceItemsBusiness iIB;
+        private  InvoiceDTO iDTO;
+
+        private void Setup()
+        {
+            List<string> codes = new List<string>();
+                
+            iDTO = new InvoiceDTO()
+            {
+                CustomerId = 1,
+                Codes = codes
+            };
+
+            var invoiceItem = new InvoiceItem()
+            {
+                ProductCode = "Product1"
+            };
+            List<InvoiceItem> invoiceItems = new List<InvoiceItem> { invoiceItem };
+            mock.Setup(x => x.AddInvoiceItems(invoiceItems)).Returns(1);
+            iIB = new InvoiceItemsBusiness(mock.Object);
+        }
+
+
+
+        [Fact]
+        public void TestingCreateInvoiceItemNotEmpty()
+        {
+            this.Setup();
+            var result = iIB.CreateInvoiceItems(1, iDTO);
+            Assert.IsType<ResponseDTO>(result);
+        }
+    }
+}
+
